dd0e88d78cb705287775bf7ccce179860e1c356c
Ameer Arsala <ameer.arsala03@gmail.com>
2024-10-03 15:58:13 -0700

    ran

--- src/pages/_components/LearnMore.svelte
+++ src/pages/_components/LearnMore.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { typewriterFinished } from "@data/client/stores";
-  import lambdinox from "@assets/images/lambdinox.svg"
+  import ranRocket from "@assets/images/ran/ran-icon-bgblack.png"
 </script>
 
 {#if $typewriterFinished}
   <div class="flex flex-col items-center justify-center text-white">
-    <a href="https://anemo.ai" target="_blank" class="relative inline-flex h-32 w-32 overflow-hidden rounded-full p-[2px] hover:outline-none hover:ring-2 hover:ring-slate-400 hover:ring-offset-2 hover:ring-offset-slate-50">
+    <a href="https://ran.so" target="_blank" class="relative inline-flex h-32 w-32 overflow-hidden rounded-full p-[2px] hover:outline-none hover:ring-2 hover:ring-slate-400 hover:ring-offset-2 hover:ring-offset-slate-50">
       <span class="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
 
       <span class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 text-sm font-medium text-white backdrop-blur-3xl">
-        <img src={lambdinox.src} alt="lambdinox" class="rounded-full h-fit w-fit" />
+        <img src={ranRocket.src} alt="RAN Icon" class="rounded-full h-fit w-fit" />
       </span>
     </a>
 
